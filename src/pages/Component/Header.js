99eed434b7e53c4ea41defdf06096f6597d1295d
ernstpf <phil@philernst.com>
2022-07-27 10:18:17 -0400

    Cleanup

--- src/pages/Component/Header.js
+++ src/pages/Component/Header.js
@@ -1,35 +1,25 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import ThemeLogo from "./../../assets/Images/ThemeLogo.png";
 import Links from "./Links";
 // import Links from './Link';
 const Header = () => {
   return (
     <>
       <div className="header">
-        
         <Navbar collapseOnSelect expand="lg">
-         
-            {/* <Navbar.Brand href="#home" className="logo">
-              <img
-                src={ThemeLogo}
-                alt="themeLogo"
-                className="img-fluid mx-auto"
-              />
-            </Navbar.Brand> */}
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end nav-menu">
-              <Nav className="me-auto font container">
-                <Links href="#Home">Home</Links>
-                <Links href="#About">About</Links>
-                {/* <Links href="#Service">Service</Links> */}
-                <Links href="#Experience">Experience</Links>
-                <Links href="#Skills">Skills</Links>
-                {/* <Links href="#Potfolio"> Portfolio</Links>
-                <Links href="#Blog">Blog</Links> */}
-                <Links href="#Contact">Contact</Links>
-              </Nav>
-            </Navbar.Collapse>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-end nav-menu"
+          >
+            <Nav className="me-auto font container">
+              <Links href="#Home">Home</Links>
+              <Links href="#About">About</Links>
+              <Links href="#Experience">Experience</Links>
+              <Links href="#Skills">Skills</Links>
+              <Links href="#Contact">Contact</Links>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
       </div>
     </>
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,12 +4,8 @@ import Header from "./Component/Header";
 import HeaderBanner from "./Component/Components/HeaderBanner";
 import Aboutus from "./Component/Components/About";
 import "./../index.css";
-import Service from "./Component/Components/Service";
 import Experience from "./Component/Components/Experience";
 import EducationSkills from "./Component/Components/EducationSkills";
-import MyPortfolio from "./Component/Components/MyPortfolio";
-import RecentArtical from "./Component/Components/RecentArtical";
-import CustomerReview from "./Component/Components/CustomerReview";
 import ScrollButton from "./Component/ScrollButton";
 const Index = () => {
   return (
@@ -17,12 +13,8 @@ const Index = () => {
       <Header />
       <HeaderBanner />
       <Aboutus />
-      {/* <Service /> */}
       <Experience/>
       <EducationSkills/>
-      {/* <MyPortfolio/> */}
-      {/* <CustomerReview/> */}
-      {/* <RecentArtical/> */}
       <Footer />
       <ScrollButton/>
     </div>
