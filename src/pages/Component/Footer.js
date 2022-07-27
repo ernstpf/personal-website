e617993675979838b35ceed34d83b3293c33f881
ernstpf <phil@philernst.com>
2022-07-27 12:26:45 -0400

    Update Footer content

--- src/pages/Component/Footer.js
+++ src/pages/Component/Footer.js
@@ -1,8 +1,6 @@
 import { Component } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { BsTelephone } from "react-icons/bs";
-import { BsEnvelope } from "react-icons/bs";
-import { BiMap } from "react-icons/bi";
+import { BsGithub, BsLinkedin, BsEnvelope } from "react-icons/bs";
 export default class Footer extends Component {
   render() {
     return (
@@ -10,32 +8,13 @@ export default class Footer extends Component {
         <footer className="footer headding" id="Contact">
           <div className="footer-head">
             <Container>
-              {/* <div className="py-5 text-center">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d106.8195613507864!3d-6.194741395493371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5390917b759%3A0x6b45e67356080477!2sPT%20Kulkul%20Teknologi%20Internasional!5e0!3m2!1sen!2sid!4v1601138221085!5m2!1sen!2sid"
-                  width="100% auto"
-                  height="350px"
-                  frameBorder="0"
-                  title="Iframe"
-                  aria-hidden="false"
-                  tabIndex="0"
-                />
-              </div> */}
-
               <Row>
                 <Col lg={6} md={12}>
                   <div className="f-left">
                     <div className="f-content">
                       <h1 className="F-contact">Contact Me</h1>
-                      {/* <h1 className="f-text">Let's Discuss Your project</h1> */}
                     </div>
                     <div className="">
-                      {/* <h6 className="f-icon">
-                        <i className="icon">
-                          <BsTelephone size={20} />
-                        </i>
-                        <span>+911234567890</span>
-                      </h6> */}
                       <h6 className="f-icon">
                         <i className="icon">
                           <BsEnvelope size={20} />
@@ -44,13 +23,13 @@ export default class Footer extends Component {
                       </h6>
                       <h6 className="f-icon">
                         <i className="icon">
-                          <BsEnvelope size={20} />
+                          <BsLinkedin size={20} />
                         </i>
                         <span>linkedin.com/in/phil-ernst</span>
                       </h6>
                       <h6 className="f-icon">
                         <i className="icon">
-                          <BsEnvelope size={20} />
+                          <BsGithub size={20} />
                         </i>
                         <span>github.com/ernstpf</span>
                       </h6>
